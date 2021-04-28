8325bddf5a040d6b1f5b888a6703d3c80e3ba298
Divine Zvenyika <dzbusiness01@gmail.com>
2021-04-28 17:55:29 +0200

    my first website

---
+++ server.py
@@ -0,0 +1,24 @@
+import os
+from flask import request, jsonify
+from tinydb import TinyDB, Query
+from flask import Flask, render_template
+
+db_query = Query()
+table = TinyDB(os.path.join(os.getcwd(), "static", "db", "messages.db")).table("newmessages")
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+  return render_template("main.html")
+
+
+@app.route("/newmessage", methods=["GET"])
+def newmesage():
+  table.insert(request.form)
+  return jsonify({"status": True})
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
