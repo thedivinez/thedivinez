c2240884ff749aecf8ab3effc77da1a6462664a6
Divine Zvenyika <dzbusiness01@gmail.com>
2021-05-07 11:25:37 +0200

    make it better

--- server.py
+++ server.py
@@ -1,16 +1,14 @@
-import os
+import pymongo
 from pytz import timezone
 from datetime import datetime
 from twilio.rest import Client
-from tinydb import TinyDB, Query
 from flask import request, jsonify
 from flask import Flask, render_template
 
-db_query = Query()
-table = TinyDB(os.path.join(os.getcwd(), "static", "db", "messages.db")).table("newmessages")
-
 app = Flask(__name__)
 app.secret_key = "s-u-p-e-r-s-e-c-r-e-t-k-e-y"
+connection = f"mongodb+srv://divine01:imwinning@splitcluster.aui7k.azure.mongodb.net/thedivinez?retryWrites=true&w=majority"
+table = pymongo.MongoClient(connection).thedivinez
 
 
 def todaydate():
@@ -20,15 +18,14 @@ def todaydate():
 
 
 def sendmeanotification(job):
+  table.jobs.insert_one(job)
   auth_token = "bbcda324a4f20c35d8f6a81df44e028a"
   account_sid = "ACc255075c59b5e719afc127c5c8d9437b"
-  table.insert(job)
   client = Client(account_sid, auth_token)
-  client.messages.create(
-      body=f"You have 1 new job offer from {job['name']}. Check it out http://thedivinez.online/jobs",
-      from_='+14133388566',
-      to='+263786854223')
-  return "Thank you for getting in touch, your message has been submited, Divine will get back to you as soon as possible."
+  client.messages.create(body=f"You have 1 new job offer from {job['name']} ~ {job['email']}",
+                         from_='+14133388566',
+                         to='+263786854223')
+  return f"Thank you {job['name']} for getting in touch, Divine will get back to you as soon as possible."
 
 
 @app.route("/")
@@ -38,7 +35,7 @@ def index():
 
 @app.route("/jobs")
 def jobs():
-  return render_template("jobs.html", jobs=table.all())
+  return render_template("jobs.html", jobs=list(table.jobs.find({}, {"_id": 0})))
 
 
 @app.route("/newmessage", methods=["POST"])
