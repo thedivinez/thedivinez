0bf4fc502a6b107f101c3cdb1203a30b23f5a287
Divine Zvenyika <dzbusiness01@gmail.com>
2021-05-19 13:27:01 +0200

    make it better

--- config/source.py
+++ config/source.py
@@ -1,5 +1,6 @@
 import os, pymongo
 from dotenv import dotenv_values
+from flask_compress import Compress
 from flask import Flask, render_template
 
 stc = os.path.join(os.getcwd(), "static")
@@ -7,6 +8,8 @@ tmp = os.path.join(os.getcwd(), "templates")
 config = dotenv_values(os.path.join(os.getcwd(), ".env"))
 app = Flask(__name__, template_folder=tmp, static_folder=stc)
 table = pymongo.MongoClient(config.get("MONGO_URL")).thedivinez
+
+Compress(app)
 
 
 @app.errorhandler(Exception)
