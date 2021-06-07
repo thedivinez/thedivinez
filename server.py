cd34606624ba98f26455a571835e1a146a373b32
Divine Zvenyika <dzbusiness01@gmail.com>
2021-06-07 09:11:53 +0200

    make change tracking code

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from api.routes import web
 from config.source import app
+from flask_compress import Compress
 
 app.register_blueprint(web)
+Compress(app)
 
 if __name__ == "__main__":
   app.run(debug=True)
