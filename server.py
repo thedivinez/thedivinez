a32f3a1e8d20f7a5b3c18231a77a59bfab8c18bf
Divine Zvenyika <dzbusiness01@gmail.com>
2021-07-29 22:26:14 +0200

    make it better

--- server.py
+++ server.py
@@ -1,15 +1,34 @@
-from main.routes import web
+from os import getcwd
+from flask import request
 from flask_cors import CORS
-from config.source import app
+from config.source import table
+from main.processor import Engine
 from flask_compress import Compress
 from config.source import app, socket
+from flask import render_template as show
 from api.telejoiner.main import TeleJoiner
-
-app.register_blueprint(web)
 
 CORS(app)
 Compress(app)
 socket.on_namespace(TeleJoiner(namespace="/telejoiner"))
+
+
+@app.route("/")
+def index():
+    portfoliodata = table.configs.find_one({"section": "portfolio"}, {"_id": 0})
+    return show("main.html", portfoliodata=portfoliodata.get("data")), 200
+
+
+@app.route("/apps/<target>", methods=["GET"])
+def gotopage(target):
+    return show(f"projects/{target}.html"), 200
+
+
+@app.route("/<target>", methods=["GET", "POST"])
+def decidewhattodo(target):
+    func = getattr(Engine, target) if not target == "favicon.ico" else getcwd
+    return func() if request.method == "POST" else show(f"{target}.html", data=func())
+
 
 # === start the server ===
 if __name__ == "__main__":
